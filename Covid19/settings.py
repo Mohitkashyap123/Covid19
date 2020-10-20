106fd16cfa1eb24683eb9da53d37f44233fe15ad
Mohit <mohitkashyap656@gmail.com>
2020-10-20 17:05:00 +0530

    changed settings.py

--- Covid19/settings.py
+++ Covid19/settings.py
@@ -82,7 +82,11 @@ DATABASES = {
     }
 }
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+import dj_database_url
 
+db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
